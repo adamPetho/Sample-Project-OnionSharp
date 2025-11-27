c6c814f3d4340f72b9e8f4ab0ef62b71609c7837
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-11-27 20:51:05 +0100

    add console app sample

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Sample-Project-OnionSharp
+# Sample-Project for OnionSharp nuget package
+
+Simple console application to showcase how to:
+- Create TorSettings
+- Create and Start TorProcessManager
+- Create OnionHttpClientFactory
+- Create and use HttpClient from OnionHttpClientFactory
---
+++ Sample-Project-OnionSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using OnionSharp.Microservices;
+using OnionSharp.Tor;
+using OnionSharp.Tor.Models;
+
+namespace Sample_Project_OnionSharp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var dataDir = EnvironmentHelpers.GetDataDir("Sample-Project");
+
+            var torSetting = new TorSettings(dataDir,
+                distributionFolderPath: EnvironmentHelpers.GetFullBaseDirectory(),
+                terminateOnExit: true,
+                TorMode.Enabled,
+                socksPort: 37155,
+                controlPort: 37156);
+
+            TorProcessManager torProcessManager = new TorProcessManager(torSetting);
+            await torProcessManager.StartAsync(attempts: 3, CancellationToken.None);
+
+            var onionClientFactory = new OnionHttpClientFactory(torSetting.SocksEndpoint.ToUri("socks5"));
+
+            var blockstreamClient = onionClientFactory.CreateClient("blockstream.info");
+
+            var response = await blockstreamClient.GetAsync("http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/api/mempool/recent");
+
+            Console.WriteLine($"Response Status: {response.StatusCode}");
+        }
+    }
+}
