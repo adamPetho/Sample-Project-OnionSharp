5687de87fd72773be25f54ed6ebfd1c1975d3e33
adamPetho <45069029+adamPetho@users.noreply.github.com>
2025-11-28 19:19:47 +0100

    add test controller

---
+++ ASP.NET-Core-Sample-Project-OnionSharp/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Sample_Project_OnionSharp.Controllers
+{
+    public class TestController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public TestController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("test")]
+        public async Task<IActionResult> Index()
+        {
+            var onionhttpClient = _httpClientFactory.CreateClient("Blockstream-info");
+
+            var response = await onionhttpClient.GetAsync("http://explorerzydxu5ecjrkwceayqybizmpjjznk5izmitf2modhcusuqlid.onion/api/mempool/recent");
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine($"Response Status: {response.StatusCode}");
+
+            return Ok(responseContent);
+        }
+    }
+}
